928d9802b0680f17ecce2ed1526601431b395830
jordan williamson <williamsonjordan05@gmail.com>
2024-07-05 22:18:30 -0600

    changed dates to show the month and year and added different styling for the page

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,13 +1,14 @@
 import './Nav.css'
 import home from '../images/icons8-home-150.png'
 import ticket from '../images/icons8-movie-ticket-100.png'
+import tomato from '../images/icons8-tomato-64.png'
 
 const Nav = () => {
     return (
         <div className='navContainer'>
-            <img src={ticket} />
-            <h1>Rancid Tomatillos!</h1>
-            <img alt='home icon' src={home} />
+            <img className='ticket' alt='ticket icon'src={ticket} />
+            <h1>Rancid Tomatillos! <img className='tomatillo' alt='tomato' src={tomato}/> </h1>
+            <img className='home'alt='home icon' src={home} />
         </div>
     )
 }
